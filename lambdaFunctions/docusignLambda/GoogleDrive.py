fd8fe0c8dd552fab06d03626af9e21ff5bea68ee
KevinHa48 <kh220kh@gmail.com>
2023-02-05 23:30:58 -0500

    Finished implementing docusign webhook

--- lambdaFunctions/docusignLambda/GoogleDrive.py
+++ lambdaFunctions/docusignLambda/GoogleDrive.py
@@ -1,27 +1,31 @@
-from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
-from googleapiclient.http import MediaFileUpload
-import io, base64, os
+from googleapiclient.http import MediaIoBaseUpload
+from google.oauth2.service_account import Credentials
+
+import io, os, base64
 
 class GoogleDrive():
-    def __init__(self, credentials_filepath='credentials.json'):
-        if not os.path.exists(credentials_filepath):
-            raise Exception("credentials.json not found")
+    def __init__(self):
         self._SCOPES = [
             'https://www.googleapis.com/auth/drive',
             'https://www.googleapis.com/auth/drive.file'
         ]
-        self.credentials = Credentials.from_authorized_user_file(credentials_filepath, scopes=self._SCOPES) #TODO doesn't work
+        self.credentials = None
+        self.check_credentials() #TODO doesn't work
         self.service = build('drive', 'v3', credentials=self.credentials)
-        
-        
+
+    def check_credentials(self, credentials_filepath='service_account.json'):
+        if not os.path.exists(credentials_filepath):
+            raise Exception("service_account.json not found")
+        self.credentials = Credentials.from_service_account_file(credentials_filepath)
+
     def resumable_upload_to_drive(self, file_data: str, file_name: str, file_mimetype: str, is_base64 : bool = False):
-        if is_base64:
-            file_data = base64.b64decode(file_data).decode('utf-8')
+        file_data = base64.b64decode(file_data)
         file_stream = io.BytesIO(file_data)
-        media = MediaFileUpload(file_stream, mimetype=file_mimetype, resumable=True)
+        media = MediaIoBaseUpload(file_stream, mimetype=file_mimetype, resumable=True)
         file_metadata = {
-            'name': file_name
+            'name': file_name,
+            'parents': ['17TvqjY62dW_hc0_K-b6C8-mbk930pbzC'] # TODO: replace this with an actual official CPAC shared folder
         }
         file = self.service.files().create(body=file_metadata, media_body=media, fields='id').execute()
         return file.get("id")
