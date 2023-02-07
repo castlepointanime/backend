df12eb70718bcaa200d60bb905396c60eadbd757
George Carvellas <gcarvel264@gmail.com>
2023-02-06 23:55:33 -0500

    added code stub

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from datetime import datetime
+from create_contract import create_contract
 
 app = Flask(__name__)
 CORS(app)
@@ -8,3 +9,7 @@ CORS(app)
 @app.route('/')
 def main():
     return jsonify(datetime.now())
+
+@app.route('/createContract', methods=['POST'])
+def create_contract_route():
+    return create_contract(request)
---
+++ create_contract.py
@@ -0,0 +1,2 @@
+def create_contract(request):
+    return {'error': "NOT IMPLEMENTED YET"}, 500 #TODO
