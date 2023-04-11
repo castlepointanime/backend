0079e9a79ab17281a240727efa3e42fa179c34ff
gcarvellas <gcarvel264@gmail.com>
2023-04-11 17:31:35 -0400

    added GET /health and GET /me

--- routes/me.py
+++ routes/me.py
@@ -1,14 +1,18 @@
 from flask import Blueprint, jsonify
 from flask_cognito import  cognito_auth_required, current_user, current_cognito_jwt, cognito_group_permissions
+from flask import current_app
+from validation import verify_id_token
 
 me = Blueprint('me', __name__)
 
 @me.route('/me', methods=["GET"])
 @cognito_auth_required
 def get():
+    verify_id_token()
+
     return jsonify({
-        'username': str(current_cognito_jwt['username']),
-        '_id': str(current_user)
+        'name': str(current_user),
+        'email': str(current_cognito_jwt['email'])
     })
 
 @me.route('/me', methods=["PATCH"])
---
+++ utils/validation.py
@@ -0,0 +1,9 @@
+
+from flask_cognito import cognito_auth_required, current_user, current_cognito_jwt, cognito_group_permissions
+
+def verify_id_token():
+    """
+    Returns 400 if header token is not id
+    """
+    if current_cognito_jwt['token_use'] == "id":
+        return jsonify({'error': "Header must contain ID token"}), 400
